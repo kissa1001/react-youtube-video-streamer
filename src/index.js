7ef7a06cf90e548bd05e8d0c17bcf0c891a5838a
kissa1001 <kissa101295@gmail.com>
2016-11-17 18:16:45 -0800

    added some styling

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCj2H5wPLcLtbyupgI7Q3ptwLYr76IBGek';
 
@@ -10,11 +11,16 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = {
+             videos: [],
+             selectedVideo: null
+         };
 
         YTSearch({key: API_KEY, term: 'javascript'}, (videos) => {
-            this.setState({ videos });
-            // this.setState({ videos: videos });
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
@@ -22,7 +28,10 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                <VideoList videos = { this.state.videos } />
+                <VideoDetail video = {this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect = { selectedVideo => this.setState({selectedVideo}) }
+                    videos = { this.state.videos } />
             </div>
         );
     }
